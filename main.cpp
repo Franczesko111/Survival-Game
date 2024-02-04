64a93c8a1b5485aa7bfbe4ded4109958f7a95205
Franczesko111 <95369030+Franczesko111@users.noreply.github.com>
2024-02-04 10:24:02 +0100

    config window
    configuration variables so its easier to read

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <raylib.h>
 
+//Configuration Variables
+const int SCREEN_WIDTH = 512;
+const int SCREEN_HEIGHT = 480;
+const int GAME_FPS = 60;
+const char* GAME_TITLE = "Survival Game Test";
+
 int main()
 {
-    InitWindow(512, 480, "Survival Game Test");
-    SetTargetFPS(60);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
+    SetTargetFPS(GAME_FPS);
 
     while(WindowShouldClose() == false)
     {
